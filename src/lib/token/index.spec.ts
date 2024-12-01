12bacafcb247f816659245e5147f2478d6184eae
Ernesto <ernestohlacalle@gmail.com>
2024-12-01 17:58:27 +0100

    fix token sort algorithm

--- src/lib/token/index.spec.ts
+++ src/lib/token/index.spec.ts
@@ -3,6 +3,13 @@ import Token from "."
 
 describe("Token", () => {
   describe("lowerThan", () => {
+    test("source token greater than target token", () => {
+      const sourceToken = new Token([4, 2])
+      const targetToken = new Token([4, 1])
+
+      expect(sourceToken.lowerThan(targetToken)).toBe(false)
+    })
+
     test("source token is lower than target token", () => {
       const sourceToken = new Token([4, 11, 0])
       const targetToken = new Token([5])
@@ -15,6 +22,103 @@ describe("Token", () => {
       const targetToken = new Token([4, 10])
 
       expect(sourceToken.lowerThan(targetToken)).toBe(false)
+    })
+
+    test("source token is greather than target token", () => {
+      const sourceToken = new Token([5, 1])
+      const targetToken = new Token([4, 10])
+
+      expect(sourceToken.lowerThan(targetToken)).toBe(false)
+    })
+
+    test("source token is greater than target token", () => {
+      const sourceToken = new Token([4, 1, 70])
+      const targetToken = new Token([4, 1, 0])
+
+      expect(sourceToken.lowerThan(targetToken)).toBe(false)
+    })
+
+    test("source token is lower than target token", () => {
+      const sourceToken = new Token([4, 1, 70, 1])
+      const targetToken = new Token([4, 1, 70])
+
+      expect(sourceToken.lowerThan(targetToken)).toBe(false)
+    })
+  })
+
+  describe('greatherThan', () => {
+    test("source token greater than target token", () => {
+      const sourceToken = new Token([4, 2])
+      const targetToken = new Token([4, 1])
+
+      expect(sourceToken.greatherThan(targetToken)).toBe(true)
+    })
+
+    test("source token is lower than target token", () => {
+      const sourceToken = new Token([4, 0])
+      const targetToken = new Token([4, 1])
+
+      expect(sourceToken.greatherThan(targetToken)).toBe(false)
+    })
+
+    test("source token is lower than target token", () => {
+      const sourceToken = new Token([4, 0])
+      const targetToken = new Token([4, 1])
+
+      expect(sourceToken.greatherThan(targetToken)).toBe(false)
+    })
+
+    test("source token is greather than target token", () => {
+      const sourceToken = new Token([5])
+      const targetToken = new Token([4, 10])
+
+      expect(sourceToken.greatherThan(targetToken)).toBe(true)
+    })
+
+    test("source token is lower than target token", () => {
+      const sourceToken = new Token([4, 1])
+      const targetToken = new Token([4, 1, 70,1])
+
+      expect(sourceToken.greatherThan(targetToken)).toBe(false)
+    })
+  })
+
+  describe('equal', () => {
+    test('same numbers in the same position', () => {
+      const sourceToken = new Token([4, 1, 70])
+      const targetToken = new Token([4, 1, 70])
+  
+      expect(sourceToken.equal(targetToken)).toBe(true)
+    })
+
+    test('same numbers in the different position', () => {
+      const sourceToken = new Token([1, 4, 70])
+      const targetToken = new Token([4, 1, 70])
+  
+      expect(sourceToken.equal(targetToken)).toBe(false)
+    })
+  })
+
+  describe('lowerThanOrEqual', () => {
+    test("source token is lower than target token", () => {
+      const sourceToken = new Token([4, 11, 0])
+      const targetToken = new Token([5])
+
+      expect(sourceToken.lowerThanOrEqual(targetToken)).toBe(true)
+    })
+
+    test("source token is equal to target token", () => {
+      const sourceToken = new Token([4, 0])
+      const targetToken = new Token([4, 0])
+
+      expect(sourceToken.lowerThanOrEqual(targetToken)).toBe(true)
+    })
+
+    test("source token is greater to target token", () => {
+      const sourceToken = new Token([4, 2])
+      const targetToken = new Token([4, 1])
+
+      expect(sourceToken.lowerThanOrEqual(targetToken)).toBe(false)
     })
   })
 })
--- src/lib/token/index.ts
+++ src/lib/token/index.ts
@@ -1,21 +1,53 @@
 class Token {
   protected values: Array<number>
+  public signature: number
   
   constructor(values: Array<number>) {
     this.values = values
+    this.signature = this.generateSignature()
   }
-  
+
+  private generateSignature() {
+    let signature = 0
+
+    for(let i = this.values.length; i > 0; i--) {
+      signature = (i * this.values[this.values.length - i]) + signature
+    }
+
+    return signature
+  }
+
+  greatherThan(target: Token) {
+    return this.compare(target, (sourceValue, targetValue) => sourceValue > targetValue)
+  }
+
   lowerThan(target: Token) {
+    return this.compare(target, (sourceValue, targetValue) => sourceValue < targetValue)
+  }
+
+  equal(target: Token) {
+    return this.signature == target.signature
+  }
+
+  lowerThanOrEqual(target: Token) {
+    return this.equal(target) || this.lowerThan(target)
+  }
+
+  compare(target: Token, condition: (x: number, y: number) => boolean ) {
     let meetCondition = false
     const length = this.values.length < target.values.length ? this.values.length : target.values.length
-
 
     for(let i = 0; i < length; i++) {
       const sourceValue = this.values[i]
       const targetValue = target.values[i]
 
-      if (sourceValue < targetValue) {
+      if(sourceValue == targetValue) { continue }
+      
+      if (condition(sourceValue, targetValue)) {
         meetCondition = true
+        break
+      } else {
+        meetCondition = false
         break
       }
     }
