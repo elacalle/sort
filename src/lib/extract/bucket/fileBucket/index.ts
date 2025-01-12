e29c7d6f43558440cf3ad22d587f6479c14708a3
Ernesto <ernestohlacalle@gmail.com>
2025-01-12 19:21:33 +0100

    add file reader to file bucket class

--- src/lib/extract/bucket/fileBucket/index.ts
+++ src/lib/extract/bucket/fileBucket/index.ts
@@ -1,33 +1,63 @@
 import { IBucket } from ".."
+import fileReader, {Token as FileToken} from "../../../fileReader"
 import Token from "../../../token"
+import fs from "fs"
 
 class FileBucket implements IBucket {
   private id: number = 0
   private tokens: Token[] = []
-  private index: number = 0
+  private read: () => Promise<FileToken>
+  private currentToken: Token
+  private done = false
   
   constructor(id: number) {
     this.id = id
+    this.currentToken = new Token([])
+  
+    const { read } = fileReader(this.path())
+    this.read = read
   }
 
   addBulk(tokens: Token[]) {
     this.tokens = [...tokens]
   }
   
-  next() {
-    this.index += 1
+  async next() {
+    if(this.done) { return }
+
+    const { done, token } = await this.read()
+
+    if(token && !done) {
+      this.currentToken = Token.fromString(token)
+    }
+
+    this.done = done
   }
   
   hasNext() {
-    return this.index < this.tokens.length
+    return !this.done
   }
   
   dump() {
-    // This class doest not have a external IO
+    this.tokens.forEach((token) => this.write(`${token.toString()}\n`))
   }
   
   current() {
-    return this.tokens[this.index]
+    return this.currentToken
+  }
+
+  drop() {
+    if(fs.existsSync(this.path())) {
+      fs.unlinkSync(this.path())
+    }
+  }
+
+  private write(data: string) {
+    fs.appendFileSync(this.path(), data)
+  }
+
+  private path() {
+    return `/tmp/sort_${this.id}.txt`
   }
 }
 
