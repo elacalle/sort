5f7064ace62a47393c52d82c45b654f97c9403b1
Ernesto <ernestohlacalle@gmail.com>
2025-01-19 17:14:06 +0100

    remove useless string interpolation

--- src/lib/path/resolve/index.ts
+++ src/lib/path/resolve/index.ts
@@ -2,7 +2,7 @@ import { existsSync } from "fs"
 import { isAbsolute, resolve } from "path"
 
 const resolvePath = (path: string) => {
-  const filePath = isAbsolute(path) ? path : `${resolve(process.cwd(), path)}`
+  const filePath = isAbsolute(path) ? path : resolve(process.cwd(), path)
 
   if(!existsSync(filePath)) throw new Error('File does not exist')
 
