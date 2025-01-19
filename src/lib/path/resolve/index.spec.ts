b5dc7f2082d581a37ba3b4d9890f83232665c899
Ernesto <ernestohlacalle@gmail.com>
2025-01-19 17:00:14 +0100

    path resolver

---
+++ src/lib/path/resolve/index.spec.ts
@@ -0,0 +1,46 @@
+import mock from "mock-fs";
+import { describe, test, beforeEach, afterEach, expect, vi } from "vitest";
+import resolvePath from ".";
+
+describe('path', () => {
+  beforeEach(() => {
+    vi.spyOn(process, 'cwd').mockReturnValue('/tmp/')
+
+    mock({
+      '/tmp/foo/bar/foobar.txt': 'Hello world'
+    })
+  })
+
+  test('path is absolute returns same path', () => {
+    expect(resolvePath("/tmp/foo/bar/foobar.txt")).toBe("/tmp/foo/bar/foobar.txt")
+  })
+
+  test('when a path is relative resolves to the absolute path', () => {
+    expect(resolvePath("foo/bar/foobar.txt")).toBe("/tmp/foo/bar/foobar.txt")
+  })
+
+  test('when a path is relative resolves to the absolute path', () => {
+    vi.spyOn(process, 'cwd').mockReturnValue('/tmp/foo/bar/baz')
+    mock({
+        '/tmp/foo/foobar.txt': 'asdad',
+        '/tmp/foo/bar/baz': {}
+    })
+
+    expect(resolvePath("../../../foo/foobar.txt")).toBe("/tmp/foo/foobar.txt")
+  })
+
+  test('when the file doesnt exists', () => {
+    vi.spyOn(process, 'cwd').mockReturnValue('/tmp/foo/bar/baz')
+    mock({
+        '/tmp/foo': {},
+        '/tmp/foo/bar/baz': {}
+    })
+
+    expect(() => resolvePath("../../../foo/foobar.txt")).toThrow('File does not exist')
+  }) 
+
+  afterEach(() => {
+    vi.spyOn(process, 'cwd').mockRestore()
+    mock.restore()
+  })
+})
---
+++ src/lib/path/resolve/index.ts
@@ -0,0 +1,12 @@
+import { existsSync } from "fs"
+import { isAbsolute, resolve } from "path"
+
+const resolvePath = (path: string) => {
+  const filePath = isAbsolute(path) ? path : `${resolve(process.cwd(), path)}`
+
+  if(!existsSync(filePath)) throw new Error('File does not exist')
+
+  return filePath
+}
+
+export default resolvePath
