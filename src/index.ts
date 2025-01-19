dcaccda371279034149b8c1879f6b150426d9e77
Ernesto <ernestohlacalle@gmail.com>
2025-01-19 17:01:38 +0100

    add commands

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
 import Loader from "./lib/extract/loader"
-import fs, { read } from "fs"
-import readline from "readline"
+import fs from "fs"
+import { program } from 'commander'
 import bucketFactory from "./lib/extract/bucket/factory";
 import { Run, RunFactory } from "./lib/merge/run";
 import Evaluator from "./lib/merge/evaluator";
 import Director from "./lib/merge/director";
 import StdOutput from "./lib/output/stdOutput";
+import resolvePath from "./lib/path/resolve";
 
 const main = async () => {
+  let filePath = ''
+
+  program.name("sort").description('sort UTF-8 files').version("0.1")
+  program.helpOption('-h', 'help')
+
+  program.argument('<file>', 'file path').action((path) => {
+    filePath = path
+  })
+
+  program.parse()
+
+  try {
+    filePath = resolvePath(filePath)
+  } catch {
+    process.stderr.write('missing file\n')
+    process.exitCode = 0
+    process.exit()
+  }
+
   const runs: Run[] = []
   const evaluator = new Evaluator()
   const stream = fs.createReadStream("words.txt");
