9b34ca90f9ec485eb360d0fdcb3eef73c543fcff
Ernesto <ernestohlacalle@gmail.com>
2025-01-25 23:10:19 +0100

    fix director test in order to use real UTF codes

--- src/lib/merge/director/index.test.ts
+++ src/lib/merge/director/index.test.ts
@@ -8,7 +8,7 @@ import DummyBucket from '../../extract/bucket/dummyBucket'
 
 describe('Director', () => {
   describe('validate', () => {
-    test('throws and error when the runner id is duplicated', () => {
+    test('throws and error when the runner id is duplicated', async () => {
       const evaluator = new Evaluator()
       const runs: Run[] = []
 
@@ -27,37 +27,45 @@ describe('Director', () => {
       const dummy = new DummyOutput()
 
       const director = new Director(evaluator, runs, dummy)
-      expect(() => director.call()).toThrow('Run id 1 its duplicated')
+      await expect(director.call()).rejects.toThrow('Run id 1 its duplicated')
     })
   })
 
-  test('order the runs values', () => {
+  test('order the runs values', async () => {
     const evaluator = new Evaluator()
     const runs: Run[] = []
 
     const firstRun = new Run(1, evaluator)
     const firstDummyBucket = new DummyBucket(0)
-    firstDummyBucket.addBulk([new Token([4, 1]), new Token([5])])
+    firstDummyBucket.addBulk([new Token([99, 98]), new Token([100])])
     firstRun.setBucket(firstDummyBucket)
     runs.push(firstRun)
 
     const secondRun = new Run(2, evaluator)
     const secondDummyBucket = new DummyBucket(1)
-    secondDummyBucket.addBulk([new Token([4, 2]), new Token([5, 1])])
+    secondDummyBucket.addBulk([new Token([99, 100]), new Token([100, 99])])
     secondRun.setBucket(secondDummyBucket)
     runs.push(secondRun)
 
     const thirdRun = new Run(3, evaluator)
     const thirdDummyBucket = new DummyBucket(2)
-    thirdDummyBucket.addBulk([new Token([1, 2]), new Token([3])])
+    thirdDummyBucket.addBulk([new Token([97, 98]), new Token([98])])
     thirdRun.setBucket(thirdDummyBucket)
     runs.push(thirdRun)
 
     const dummy = new DummyOutput()
+    const spy = vi.spyOn(dummy, 'write')
 
     const director = new Director(evaluator, runs, dummy)
-    director.call()
+    await director.call()
 
-    expect(dummy.output).toEqual([[1, 2], [3], [4, 1], [4, 2], [5], [5, 1]])
+    expect(spy.mock.calls).toEqual([
+      ['ab'],
+      ['b'],
+      ['cb'],
+      ['cd'],
+      ['d'],
+      ['dc']
+    ])
   })
 })
