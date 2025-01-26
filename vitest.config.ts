c38ef85cb44328c4b72ba7508eb99ed2d0b4e93f
Ernesto <ernestohlacalle@gmail.com>
2025-01-26 10:23:52 +0100

    remove comment vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
-    // Otros ajustes como la inclusión de archivos de test pueden ir aquí
-  },
-});
+    environment: 'node'
+  }
+})
