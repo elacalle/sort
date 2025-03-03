78a00514b67a54c849ae8bdc52f8a75fefe4c433
Ernesto Lacalle <36649033+elacalle@users.noreply.github.com>
2025-03-03 21:48:25 +0100

    fix: add CLI parameter to choose sort algorithm

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,33 @@ import Evaluator from './lib/merge/evaluator'
 import Director from './lib/merge/director'
 import StdOutput from './lib/output/stdOutput'
 import resolvePath from './lib/path/resolve'
+import { default as radix } from './lib/algorithms/radixsort'
+import { default as quick } from './lib/algorithms/quicksort'
+
+const SORT_STRATEGIES = {
+  quick: quick,
+  radix: radix
+}
+
+type SortTypes = keyof typeof SORT_STRATEGIES
 
 const main = async () => {
   let filePath = ''
+  let sortStrategy = quick
 
   program.name('sort').description('sort UTF-8 files').version('0.1')
   program.helpOption('-h', 'help')
 
-  program.argument('<file>', 'file path').action((path) => {
-    filePath = path
-  })
+  program
+    .argument('<file>', 'file path')
+    .option('-a, --sort [type]', 'set the algorithm to sort your collection')
+    .action((path, options) => {
+      filePath = path
+      const sortAlgorithm: SortTypes = (options['sort'] =
+        options['sort'] || 'quick')
+
+      sortStrategy = SORT_STRATEGIES[sortAlgorithm] || quick
+    })
 
   program.parse()
 
@@ -33,7 +50,7 @@ const main = async () => {
   const stream = fs.createReadStream(filePath)
 
   const bucketFileFactory = bucketFactory('file')
-  const loader = new Loader(bucketFileFactory, stream)
+  const loader = new Loader(bucketFileFactory, stream, sortStrategy)
   await loader.call()
 
   const buckets = loader.getBuckets()
